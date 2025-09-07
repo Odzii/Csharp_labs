d4ee83ff2f6f1ee94982e87b839bbf16ea1738d0
Odzii <Odzii@yandex.ru>
2025-09-08 01:04:28 +0700

    I've done first task in seventh lab

---
+++ Лаба 7/Seventh_labs/Seventh_labs/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Seventh_labs
+{
+    internal class Program
+    {
+        struct Date
+        {
+            public int day { get; set; }
+            public int month { get; set; }
+            public int year { get; set; }
+
+            // Конструктор с параметрами
+            public Date(int d, int m, int y)
+            {
+                day = d;
+                month = m;
+                year = y;
+            }
+
+            // Метод внутри структуры
+            public void Nextday(int day_count)
+            {
+                if (1 <= day & day < day_count)
+                {
+                    day++;
+                }
+                else if (day == day_count)
+                {
+                    day = 1;
+                    month++;
+                    if (month == 13)
+                    {
+                        Console.WriteLine("Ебать ты долбаеб");
+                        month = 1;
+                        year++;
+                    }
+                    else
+                        Console.WriteLine("Тебе повезло, ты же долбаеб");
+                }
+                else
+                {
+                    Console.WriteLine("Давай по новой Миша всё хуйня?");
+                    return;
+                }
+                Console.WriteLine(day + "." + month + "." + year);
+            }
+            public void Print()
+            {
+                var month_dict = new Dictionary<int, int>
+                   {
+                       {1, 31 },
+                       {2, 28 },
+                       {3, 31 },
+                       {4, 30 },
+                       {5, 31 },
+                       {6, 30},
+                       {7, 31},
+                       {8, 31},
+                       {9, 30},
+                       {10, 31},
+                       {11, 30},
+                       {12, 31}
+                   };
+                
+                if (month == 2)
+                {
+                    if ((year % 4 == 0 & year % 100 != 0) || (year % 400 == 0))
+                    {
+                        Nextday(29);
+                    }
+                    else
+                    {
+                        Nextday(28);
+                    }
+                }
+                else
+                    Nextday(month_dict[month]);
+
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*
+            Дана структура, задающая дату вида: 
+            struct date 
+            {
+            int day; 
+            int month; 
+            int year;
+            }; 
+            Пользуясь таким структурным типом, составить программу, определяющую: 
+            а) дату следующего (относительно сегодняшнего) дня,
+             */
+            Console.Write("Введите день\n");
+            int.TryParse(Console.ReadLine(), out int d);
+            Console.Write("Введите месяц\n");
+            int.TryParse(Console.ReadLine(), out int m);
+            Console.Write("Введите год\n");
+            int.TryParse(Console.ReadLine(), out int y);
+            Date get_date = new Date(d, m, y);
+            get_date.Print();
+            }
+
+    }
+}
---
+++ Лаба 7/Seventh_labs/Seventh_labs/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Seventh_labs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Seventh_labs")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b6bf2ffb-0688-4dd8-9d3f-ef0ff777c130")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
