353bc6c3d6153f62e0a263dee35a19cdea8c857b
Руслан Рустамов <odzii@yandex.ru>
2025-09-26 19:26:28 +0700

    Лаба 5 готово

--- Лаба 5/Fifth_laba/Fifth_laba/Program.cs
+++ Лаба 5/Fifth_laba/Fifth_laba/Program.cs
@@ -9,6 +9,76 @@ namespace Fifth_laba
 {
     class Program
     {
+        //  Метод позволяет создать массив размером [row, col] целочисленного типа.
+        static int[,] CreateMatrix()
+        {
+            Random rnd = new Random();
+            int col, row;
+            int[,] matrix;
+            while (true)
+            {
+                //  Ввод и проверка корректности                
+                Console.Write("Введите число столбцов массива: ");
+                if (!int.TryParse(Console.ReadLine(), out col) && col > 0)
+                {
+                    Console.WriteLine("Введите целое положительное число ");
+                    continue;
+                }
+                Console.Write("Введите число строк массива: ");
+                if (!int.TryParse(Console.ReadLine(), out row) && row > 0)
+                {
+                    Console.WriteLine("Введите целое положительное число ");
+                    continue;
+                }
+                matrix = new int[row, col];
+                for (int i = 0; i < matrix.GetLength(0); i++)  //  строки
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)  //  столбцы
+                    {
+                        int rnd_value = rnd.Next(2, 10);
+                        matrix[i, j] = rnd_value;
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Массив размером [{0}, {1}] создан\n", row, col);
+                break;
+            }
+            return matrix;
+        }
+
+        //  Данная функция выполняет конвертацию 2D массива в 1D
+        static int[] MatrixToVector(int[,] matrix)
+        {
+            int counter = 0;
+            int len = matrix.GetLength(0) * matrix.GetLength(1);  //  Получаем длину вектора
+            int[] vector = new int[len];
+            if (matrix.GetLength(0) >= 1)
+            {
+                foreach (int i in matrix)
+                {
+                    vector[counter] = i;
+                    if (counter == 0)
+                    {
+
+                        Console.Write("[" + vector[counter] + ", ");
+                        counter++;
+                        continue;
+                    }
+                    else if (counter == len - 1)
+                    {
+                        Console.Write(vector[counter] + "]\n");
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(vector[counter] + ", ");
+                        counter++;
+                    }
+                }
+            }
+            return vector;
+        }
         static double Power(double x, int n)
         {
             if (n == 0)
@@ -62,8 +132,8 @@ namespace Fifth_laba
         {
             while (true)
             {                
-                Console.WriteLine("1. Возведение в степень;" + "\n2. Дополнительное задание." 
-                    + "3. Очистить консоль; " + "\n4. Выход.");
+                Console.WriteLine("1. Возведение в степень;" + "\n2. Дополнительное задание;" 
+                    + "\n3. Очистить консоль; " + "\n4. Выход.");
 
                 int valueCase = ReadInteger("Выберите вариант из списка. ");
                 if (valueCase < 1 || valueCase > 4)
@@ -72,15 +142,17 @@ namespace Fifth_laba
                 switch (valueCase)
                 {
                     case 1:
-                        double x = ReadDouble("Введите число Х");
+                        double x = ReadDouble("Введите число Х ");
                         int n = ReadInteger("Введите степень N: ");
                         Console.WriteLine($"{x}^{n} = {Power(x, n)}");
                         break;
                     case 2:
                         //  Задание №2
+                        int[,] matrix = CreateMatrix();
+                        int[] vector = MatrixToVector(matrix);
                         int[] a = { 2, 3, 4, 5, 6 };
                         // начиная с i=2 (по условию), а в C# это индекс 1
-                        double result = RecSum(a, 1);
+                        double result = RecSum(vector, 1);
                         Console.WriteLine("Сумма = " + result);
                         break;
                     case 3:
